534d958c6bd1586a123cf40eb59d7d37d17647ec
elizawura <elizabethsampong@yahoo.com>
2025-03-18 17:06:00 +0000

    authentication added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import "dotenv/config";
 import cors from "cors";
 import adRouter from "./routes/product_route.js";
+import userRouter from "./routes/user_route.js";
 
 // MAKE DATABASE CONNECTION
 await mongoose.connect(process.env.MONGO_URI);
@@ -14,20 +15,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(adRouter);
+app.use(userRouter);
 
 // LISTEN FOR INCOMING REQUESTS
 const port = process.env.PORT || 6000;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
-
-//for the env file
-// MONGO_URI = "mongodb+srv://advert-api:bjFS0TeHLsET7Qko@cluster0.njhep.mongodb.net/advert_db"
-
-//  SAVEFILESORG_API_KEY ="1596|vAMprb3cS2NsHlL4zzbQyE1EOZZUYUOdJGKzAzco"
-
-//  PORT = 6000
-
-// #  1596|vAMprb3cS2NsHlL4zzbQyE1EOZZUYUOdJGKzAzco
-
-// # bjFS0TeHLsET7Qko
